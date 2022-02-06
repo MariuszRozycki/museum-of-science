d6b21fdadfdff6d1baf217cfa8e77d0b0f1b76f3
MariuszRozycki <mariansc83@icloud.com>
2022-02-06 11:26:09 +0100

    Add section .contact & form to script.js

--- js/script.js
+++ js/script.js
@@ -41,3 +41,93 @@ for (let element of mainElements) {
     }
   }
 }
+
+/* form */
+const createAppointment = (appointment) => {
+  console.log(appointment);
+
+  const appointmentMessage = document.querySelector('.appointment-message');
+
+
+
+  fetch('https://formspree.io/f/xnqogare', {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    mode: 'cors',
+    method: 'POST',
+    body: JSON.stringify(appointment)
+  })
+    .then(res => res.json())
+    .then(resJSON => {
+      console.log(resJSON);
+
+      let html = document.getElementsByTagName('html')[0];
+      let htmlAttr = html.getAttribute('lang');
+      console.log(htmlAttr);
+
+      appointmentMessage.classList.add('send');
+      appointmentMessage.classList.remove('failure');
+
+      if (htmlAttr === 'pl') {
+        appointmentMessage.innerText = `Dzięki ${appointment.name}. Wiadomosc wyslana!`;
+      } else if (htmlAttr === 'en') {
+        appointmentMessage.innerText = `Thank you ${appointment.name}. Your message is sent!`;
+      } else {
+        appointmentMessage.innerText = `Takk ${appointment.name}. Meldingen din er sendt!`;
+      }
+    });
+
+
+}
+
+
+const appointemntForm = document.getElementById('appointment-form');
+
+appointemntForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const appointmentPhone = document.getElementById('appointment-phone');
+  const appointmentMessage = document.querySelector('.appointment-message');
+  let formFields = document.getElementsByClassName('form-field');
+  let fieldsErrors = 0;
+  let appointment = {
+    name: document.getElementById('appointment-name').value,
+    email: document.getElementById('appointment-email').value,
+    city: document.getElementById('appointment-city').value,
+    phone: document.getElementById('appointment-phone').value,
+    message: document.getElementById('appointment-message').value,
+  }
+
+  console.log(typeof appointment.phone.value);
+
+
+  for (let i = 0; i < formFields.length; i++) {
+
+    console.log(formFields[3]);
+
+    if (formFields[i].value === '') {
+      ++fieldsErrors;
+      formFields[i].classList.add('failure');
+    } else {
+      formFields[i].classList.remove('failure');
+    }
+  }
+
+  if (fieldsErrors === 0) {
+    createAppointment(appointment);
+  } else {
+    appointmentMessage.classList.add('failure');
+
+    let html = document.getElementsByTagName('html')[0];
+    let htmlAttr = html.getAttribute('lang');
+
+    if (htmlAttr === 'pl') {
+      appointmentMessage.innerText = `Proszę, wypełnij wszystkie puste pola!`;
+
+    } else if (htmlAttr === 'en') {
+      appointmentMessage.innerText = `Fill up all empty fields!`;
+    } else {
+      appointmentMessage.innerText = `Vennligst fyll ut alle feltene!`;
+    }
+  }
+});
